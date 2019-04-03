5072fa9c8fd5585afb06bf97759e64551e9a12ba
Lemonyy-13 <45934234+Lemonyy-13@users.noreply.github.com>
2019-04-03 20:48:52 +0800

    创建后端类函数

---
+++ doc/mine/Administrator.java
@@ -0,0 +1,31 @@
+package mine;
+/**
+ * @author yourname
+ * @date 2019年4月3日 下午8:12:31
+ * 
+ */
+
+public class Administrator extends Role{
+	
+	@Override
+	public InfoList getReservationInfomation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public InfoList getReservationInfomation(ClassRoom classRoom) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/*public InfoList getReservationInfomation(Person person) {
+		return null;
+	}*/
+	
+	//public statusCode addReservationInfomation() {
+		
+	//}
+
+
+}
---
+++ doc/mine/ClassRoom.java
@@ -0,0 +1,15 @@
+package mine;
+/**
+ * @author yourname
+ * @date 2019年4月3日 下午8:04:02
+ * 
+ */
+
+public class ClassRoom {
+
+	byte[] name = new byte[64];
+	public ClassRoom() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
