75f0bb5d5d5a9e463788d044a9e106cbe87019c8
281723571@qq.com <281723571@qq.com>
2019-04-06 22:24:13 +0800

    init maven

---
+++ Oanakiaja_net/net/src/main/java/com/SA/net/App.java
@@ -0,0 +1,13 @@
+package com.SA.net;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
